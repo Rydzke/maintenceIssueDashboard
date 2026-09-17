6b51acfcee521137392eb3b41afa027a20b9c9fd
Rydzewski Damian <rydzewskidamian@proton.me>
2026-09-17 10:29:57 +0200

    simple json getter and table creator

---
+++ issueListDisplay.js
@@ -0,0 +1,18 @@
+function createNewCell(fatherElement,text) {
+    let newCell = document.createElement("td");
+    newCell.innerText = text;
+    fatherElement.appendChild(newCell);
+}
+fetch('./databaseIssue.json').then((response) => response.json()).then((databaseIssue) => {
+    let issueList = document.getElementById("issueList");
+    console.log(databaseIssue);
+    
+    databaseIssue.forEach(issue => {
+        let newRow = document.createElement("tr");
+        let namesInJSON = ["id","idApartament","category","description","dateAdd","dateSolved","priority","status"]
+        namesInJSON.forEach(namefromJSON => {
+            createNewCell(newRow, issue[namefromJSON]);
+        });
+        issueList.appendChild(newRow);
+    });
+});
